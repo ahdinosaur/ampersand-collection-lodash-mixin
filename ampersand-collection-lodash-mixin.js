54c718a244aae9d254e728ef238968b0cee98f85
Michael Williams <dinosaur@riseup.net>
2014-12-29 21:58:25 +1300

    rewrite to include more lodash functions and be easier to maintain

--- ampersand-collection-lodash-mixin.js
+++ ampersand-collection-lodash-mixin.js
@@ -1,64 +1,184 @@
 var _ = require('lodash');
-var slice = [].slice;
+var slice = Array.prototype.slice;
 var mixins = {};
 
-
 // Lodash methods that we want to implement on the Collection.
-var methods = ['forEach', 'each', 'map', 'collect', 'reduce', 'foldl',
-    'inject', 'reduceRight', 'foldr', 'find', 'detect', 'filter', 'select',
-    'reject', 'every', 'all', 'some', 'any', 'include', 'contains', 'invoke',
-    'max', 'min', 'toArray', 'size', 'first', 'head', 'take', 'initial', 'rest',
-    'tail', 'drop', 'last', 'without', 'difference', 'indexOf', 'shuffle',
-    'lastIndexOf', 'isEmpty', 'chain', 'sample'
+var methods = module.exports._methods = [
+    // arrays
+    'compact',
+    'difference',
+    ['drop', 'rest'],
+    ['findIndex', property],
+    ['find', property],
+    ['findLastIndex', property],
+    ['first', propertyOrNumber],
+    ['flatten', propertyWithFlag],
+    ['head', 'first'],
+    'indexOf',
+    ['initial', propertyOrNumber],
+    'intersection',
+    ['last', propertyOrNumber],
+    'lastIndexOf',
+    ['object', 'zipObject'],
+    'pull',
+    'range',
+    ['remove', property],
+    ['rest', propertyOrNumber],
+    'sortedIndex',
+    ['tail', 'rest'],
+    ['take', 'first'],
+    'union',
+    ['uniq', propertyWithFlag],
+    ['unique', 'uniq'],
+    ['unzip', 'zip'],
+    'without',
+    'xor',
+    'zip',
+    'zipObject',
+
+    // collections
+    ['all', 'every'],
+    ['any', 'some'],
+    'at',
+    ['collect', 'map'],
+    'contains',
+    ['countBy', property],
+    ['detect', 'find'],
+    ['each', 'forEach'],
+    ['eachRight', 'forEachRight'],
+    ['every', property],
+    ['filter', property],
+    ['find', property],
+    ['findLast', property],
+    ['findWhere', property],
+    ['foldl', 'reduce'],
+    ['foldr', 'reduceRight'],
+    'forEach',
+    'forEachRight',
+    ['groupBy', property],
+    ['include', 'contains'],
+    ['indexBy', property],
+    ['inject', 'reduce'],
+    'invoke',
+    ['map', property],
+    ['max', property],
+    ['min', property],
+    ['pluck', pluck],
+    'reduce',
+    'reduceRight',
+    ['reject', property],
+    'sample',
+    ['select', 'filter'],
+    'shuffle',
+    'size',
+    ['some', property],
+    ['sortBy', property],
+    'toArray',
+    ['where', 'filter'],
+
+    // objects
+    'isEmpty',
+    'chain',
 ];
 
-// Mix in each Lodash method as a proxy to `Collection#models`.
-_.each(methods, function (method) {
-    if (!_[method]) return;
-    mixins[method] = function () {
+//
+// Utility functions
+//
+function pluckOne(value) {
+    return function (model) {
+        return model.get ? model.get(value) : model[value];
+    };
+}
+
+function whereTest(attrs) {
+    return function (model) {
+        var value;
+        for (var key in attrs) {
+            value = pluckOne(key)(model);
+            if (attrs[key] !== value) return false;
+        }
+        return true;
+    };
+}
+
+function propertyIterator(value) {
+    if (_.isObject(value)) {
+        return whereTest(value);
+    } else if (_.isString(value)) {
+        return pluckOne(value);
+    } else {
+        return value;
+    }
+}
+
+//
+// Handlers for the various types of Lodash functions
+//
+function simple(method) {
+    return function () {
         var args = slice.call(arguments);
         args.unshift(this.models);
         return _[method].apply(_, args);
     };
-});
+}
 
-// Lodash methods that take a property name as an argument.
-var attributeMethods = ['groupBy', 'countBy', 'sortBy', 'indexBy'];
-
-// Use attributes instead of properties.
-_.each(attributeMethods, function (method) {
-    if (!_[method]) return;
-    mixins[method] = function (value, context) {
-        var iterator = _.isFunction(value) ? value : function (model) {
-            return model.get ? model.get(value) : model[value];
-        };
+function property(method) {
+    return function (value, context) {
+        var iterator = propertyIterator(value);
         return _[method](this.models, iterator, context);
     };
-});
+}
 
-// Return models with matching attributes. Useful for simple cases of
-// `filter`.
-mixins.where = function (attrs, first) {
-    if (_.isEmpty(attrs)) return first ? void 0 : [];
-    return this[first ? 'find' : 'filter'](function (model) {
-        var value;
-        for (var key in attrs) {
-            value = model.get ? model.get(key) : model[key];
-            if (attrs[key] !== value) return false;
+function propertyOrNumber(method) {
+    return function (value, context) {
+        if (_.isNumber(value)) {
+            return _[method](this.models, value, context);
+        } else {
+            var iterator = propertyIterator(value);
+            return _[method](this.models, iterator, context);
         }
-        return true;
-    });
-};
+    };
+}
 
-// Return the first model with matching attributes. Useful for simple cases
-// of `find`.
-mixins.findWhere = function (attrs) {
-    return this.where(attrs, true);
-};
+function propertyWithFlag(method) {
+    return function (flag, value, context) {
+        if (!_.isBoolean(flag) && !_.isNull(flag)) {
+            context = value;
+            value = (!_.isFunction(flag) && context && context[flag] === this.models) ? null : flag;
+            flag = false;
+        }
+        return _[method](this.models, flag, value, context);
+    };
+}
 
-// Plucks an attribute from each model in the collection.
-mixins.pluck = function (attr) {
-    return _.invoke(this.models, 'get', attr);
-};
+function pluck(method) {
+    return function (string) {
+        return _.invoke(this.models, 'get', string);
+    };
+}
+
+// Mix in each Lodash method as a proxy to `Collection#models`.
+function mixMethod(method) {
+    var handler;
+    if (_.isArray(method)) {
+        if (_.isString(method[1])) {
+            var alias = method[1];
+            handler = function (method) {
+                return function () {
+                    return mixins[alias].apply(this, arguments);
+                };
+            };
+        } else {
+            handler = method[1];
+        }
+        method = method[0];
+    } else {
+        handler = simple;
+    }
+    if (!_[method] || !handler) return;
+    mixins[method] = handler(method);
+}
+
+_.forEach(methods, mixMethod);
 
 module.exports = mixins;
